51ce4751d7859123bd3ed0368cff838befe664e8
Natalia-Balyanova <75272081+Natalia-Balyanova@users.noreply.github.com>
2022-03-08 18:23:48 +0300

    Initial commit

---
+++ src/main/java/ru/gb/CRMpsy/CrMpsyApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.CRMpsy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrMpsyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrMpsyApplication.class, args);
+	}
+
+}
